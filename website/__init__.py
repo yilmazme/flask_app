89f34742c976a2135c4cc2f2e61e9ae6340c1ed2
unknown <10myilmaz@gmail.com>
2021-10-21 14:21:31 +0300

    deployment config4

--- website/__init__.py
+++ website/__init__.py
@@ -15,7 +15,7 @@ def create_app():
         app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:postgrepba6ikha-@localhost/flaskapp'
 
     else:
-        app.debug = False
+        app.debug = True
         app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://euxwieiklbgchh:e00ad230c19ff4480182593902d73fd637beec3e305050f77d15160aa4fdedf3@ec2-54-217-15-9.eu-west-1.compute.amazonaws.com:5432/d43llm3tjc9p51'
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
